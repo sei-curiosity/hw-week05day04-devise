bd449a207cb74ab7796287588bcf667fb115cf4b
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2019-12-20 18:07:05 +0300

    removed user_id from permit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -72,6 +72,6 @@ class CarsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def car_params
-      params.require(:car).permit(:name, :model, :year, :user_id)
+      params.require(:car).permit(:name, :model, :year)
     end
 end
